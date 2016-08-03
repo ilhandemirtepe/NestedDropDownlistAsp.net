6d96e8f4db5f171729085bee40ce6ae1b2b85bff
ilhandemirtepe <han_soft@hotmail.com>
2016-08-03 15:23:31 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropFor.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <h3>nested dropdownlist entityframework</h3>
+            <table>
+                <tr>
+                    <td>Kıta Seç
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlkita" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlkita_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>ülke seç
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlulke" runat="server" Enabled="false" AutoPostBack="true" OnSelectedIndexChanged="ddlulke_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Şehir seç
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlsehir" runat="server" Enabled="false" OnSelectedIndexChanged="ddlsehir_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+
+            <h5>Seçilen Bilgi</h5>
+
+            <table>
+                <tr>
+                    <td>Kıta Adı</td>
+                    <td>
+                        <asp:Label ID="lblKita" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>Ülke Adı</td>
+                    <td>
+                        <asp:Label ID="lblUlke" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>Şehir Adı</td>
+                    <td>
+                        <asp:Label ID="lblŞehir" runat="server" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropFor
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        NestedDropdownListEntities dbEntity = new NestedDropdownListEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var country = from c in dbEntity.Continent select 
+                                  new { 
+                                      c.ContinentID,
+                                      c.ContinentName 
+                                  };
+                ddlkita.DataSource = country.ToList();
+                ddlkita.DataValueField = "ContinentID";
+                ddlkita.DataTextField = "ContinentName";
+                ddlkita.DataBind();
+                ddlkita.Items.Insert(0, "--kıta seç--");
+            }
+        }
+
+        protected void ddlkita_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ulkeNumber = Convert.ToInt16(ddlkita.SelectedValue.ToString());
+            var state = from s in dbEntity.Country where s.ContinentID.Equals(ulkeNumber) select 
+                            new {
+                                s.CountryID, 
+                                s.CountryName 
+                            };
+            ddlulke.DataSource = state.ToList();
+            ddlulke.Enabled = true;
+            ddlulke.DataValueField = "CountryID";
+            ddlulke.DataTextField = "CountryName";
+            ddlulke.DataBind();
+            ddlulke.Items.Insert(0, "--ulke seç--");
+        }
+
+        protected void ddlulke_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sehirNumber = Convert.ToInt16(ddlulke.SelectedValue.ToString());
+            var city = from c in dbEntity.City where c.CountryID.Equals(sehirNumber) select
+                           new 
+                           {
+                               c.CityID,
+                               c.CityName 
+                           };
+            ddlsehir.DataSource = city.ToList();
+            ddlsehir.Enabled = true;
+            ddlsehir.DataValueField = "CityID";
+            ddlsehir.DataTextField = "CityName";
+            ddlsehir.DataBind();
+            ddlsehir.Items.Insert(0, "--şehir seç--");
+        }
+
+        protected void ddlsehir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblKita.Text = ddlkita.SelectedItem.Text;
+            lblŞehir.Text = ddlsehir.SelectedItem.Text;
+            lblUlke.Text = ddlulke.SelectedItem.Text;
+
+        }
+    }
+}
