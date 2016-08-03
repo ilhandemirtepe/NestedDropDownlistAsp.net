69d9e37e690c837bf74ed91ef7393179e7f74de6
ilhandemirtepe <han_soft@hotmail.com>
2016-08-03 13:15:39 +0300

    Add files via upload

---
+++ KategoriModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmlakMVCWeb.Models
+{
+    public class KategoriModel
+    {
+        public int kategoriIDModel { get; set; }
+        public string kategoriNameModel { get; set; }
+    }
+}
