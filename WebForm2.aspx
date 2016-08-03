90b8d5d230cb575e7de9aa1303170fab40468b15
ilhandemirtepe <han_soft@hotmail.com>
2016-08-03 13:21:05 +0300

    Add files via upload

---
+++ WebForm2.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebDropDownListExample.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span style="font-family: Arial">Select Continent : </span>
+            <asp:DropDownList ID="ddlContinents" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlContinents_SelectedIndexChanged">
+                <asp:ListItem Text="--Select Continent--" Value=""></asp:ListItem>
+            </asp:DropDownList>
+
+            <br />
+            <br />
+            <span style="font-family: Arial">Select Country : </span>
+            <asp:DropDownList ID="ddlCountry" runat="server" AutoPostBack="true"
+                Enabled="false" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged">
+                <asp:ListItem Text="--Select Country--" Value=""></asp:ListItem>
+            </asp:DropDownList>
+
+            <br />
+            <br />
+            <span style="font-family: Arial">Select City : </span>
+            <asp:DropDownList ID="ddlCity" runat="server" AutoPostBack="true"
+                Enabled="false" OnSelectedIndexChanged="ddlCity_SelectedIndexChanged">
+                <asp:ListItem Text="--Select City--" Value=""></asp:ListItem>
+            </asp:DropDownList>
+
+            <br />
+            <br />
+            <asp:Label ID="lblResults" runat="server" Text="" Font-Names="Arial" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDropDownListExample
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddlContinents.AppendDataBoundItems = true;
+                String strConnString = ConfigurationManager.ConnectionStrings["baglan"].ConnectionString;
+                String strQuery = "select ContinentID, ContinentName from Continent";
+                SqlConnection con = new SqlConnection(strConnString);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQuery;
+                cmd.Connection = con;
+                try
+                {
+                    con.Open();
+                    ddlContinents.DataSource = cmd.ExecuteReader();
+                    ddlContinents.DataTextField = "ContinentName";
+                    ddlContinents.DataValueField = "ContinentID";
+                    ddlContinents.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
+        }
+
+        protected void ddlContinents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCountry.Items.Clear();
+            ddlCountry.Items.Add(new ListItem("--Select Country--", ""));
+            ddlCity.Items.Clear();
+            ddlCity.Items.Add(new ListItem("--Select City--", ""));
+            ddlCountry.AppendDataBoundItems = true;
+            String strConnString = ConfigurationManager.ConnectionStrings["baglan"].ConnectionString;
+            String strQuery = "select CountryID, CountryName from Country " + "where ContinentID=@ContinentID";
+            SqlConnection con = new SqlConnection(strConnString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@ContinentID",
+            ddlContinents.SelectedItem.Value);
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strQuery;
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                ddlCountry.DataSource = cmd.ExecuteReader();
+                ddlCountry.DataTextField = "CountryName";
+                ddlCountry.DataValueField = "CountryID";
+                ddlCountry.DataBind();
+                if (ddlCountry.Items.Count > 1)
+                {
+                    ddlCountry.Enabled = true;
+                }
+                else
+                {
+                    ddlCountry.Enabled = false;
+                    ddlCity.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCity.Items.Clear();
+            ddlCity.Items.Add(new ListItem("--Select City--", ""));
+            ddlCity.AppendDataBoundItems = true;
+            String strConnString = ConfigurationManager.ConnectionStrings["baglan"].ConnectionString;
+            String strQuery = "select CityID, CityName from City " +"where CountryID=@CountryID";
+            SqlConnection con = new SqlConnection(strConnString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@CountryID", ddlCountry.SelectedItem.Value);
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strQuery;
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                ddlCity.DataSource = cmd.ExecuteReader();
+                ddlCity.DataTextField = "CityName";
+                ddlCity.DataValueField = "CityID";
+                ddlCity.DataBind();
+                if (ddlCity.Items.Count > 1)
+                {
+                    ddlCity.Enabled = true;
+                }
+                else
+                {
+                    ddlCity.Enabled = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        protected void ddlCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                      lblResults.Text = "You Selected " +
+                      ddlContinents.SelectedItem.Text + " -----> " +
+                      ddlCountry.SelectedItem.Text + " -----> " +
+                      ddlCity.SelectedItem.Text;
+        }
+    }
+}
